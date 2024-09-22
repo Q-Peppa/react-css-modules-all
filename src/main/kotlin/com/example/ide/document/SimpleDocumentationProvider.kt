0791a0c31de3deec4e79e20e05b8a9ae7bcb943b
Peppa <54980372+Q-Peppa@users.noreply.github.com>
2024-09-23 01:35:55 +0800

    fix(compatible): the appendStyledCodeBlock not compatible is 233.*

--- src/main/kotlin/com/example/ide/document/SimpleDocumentationProvider.kt
+++ src/main/kotlin/com/example/ide/document/SimpleDocumentationProvider.kt
@@ -2,29 +2,38 @@ package com.example.ide.document
 
 import com.intellij.lang.css.CSSLanguage
 import com.intellij.lang.documentation.DocumentationProvider
-import com.intellij.lang.documentation.QuickDocHighlightingHelper.appendStyledCodeBlock
 import com.intellij.lang.javascript.psi.JSLiteralExpression
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.richcopy.HtmlSyntaxInfoUtil
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
 import com.intellij.psi.css.CssSelector
 import com.intellij.psi.util.PsiTreeUtil
 
 class SimpleDocumentationProvider : DocumentationProvider {
 
     private fun renderDoc(cssSelector: CssSelector): String {
-        return StringBuilder().apply {
-            appendStyledCodeBlock(cssSelector.project, CSSLanguage.INSTANCE, cssSelector.ruleset?.text?.trim() ?: "")
-        }.toString()
+        val s = StringBuilder("<pre><code>")
+        // appendStyledCodeBlock not compatible in 233.*
+        val code =cssSelector.ruleset?.block?.text?.replace(" ", "")?.replace("{","")?.replace("}","")?.trim() ?: ""
+        s.append(code)
+        s.append("</code></pre>")
+        return  s.toString()
     }
 
     override fun generateDoc(element: PsiElement?, originalElement: PsiElement?): String? {
         val origin = super.generateDoc(element, originalElement)
         element ?: return origin
 
+        if (element.parent is CssSelector) return renderDoc(element.parent as CssSelector)
+
         return when (element) {
             is CssSelector -> renderDoc(element)
             else -> {
+
                 val expression = PsiTreeUtil.findChildOfType(element, JSLiteralExpression::class.java)
                 val resolve = expression?.reference?.resolve()
+                if (resolve?.parent is CssSelector) return renderDoc(resolve.parent as CssSelector)
                 if (resolve is CssSelector) renderDoc(resolve) else origin
             }
         }
